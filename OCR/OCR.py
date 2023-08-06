21d2ddce306e4622f31c3c0448f8ccbb0b58521a
Shashank Hegde <115028650+hegdeshashank73@users.noreply.github.com>
2023-08-06 20:21:16 +0530

    Update OCR.py

--- OCR/OCR.py
+++ OCR/OCR.py
@@ -4,19 +4,18 @@ import win32com.client
 from pytesseract import Output
 from PIL import Image	
 import pyttsx3
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\shsha\AppData\Local\Tesseract-OCR\tesseract.exe'
+img1 = Image.open('test1.jpg')
+
+# describes image format in the output
+print(img1)						
+# path where the tesseract module is installed
+# converts the image to result and saves it into result variable
+result = pytesseract.image_to_string(img1)
+# write text in a text file and save it to source path
+with open('abc.txt',mode ='w') as file:	
 	
-
-
-camera=cv2.VideoCapture(1)
-
-while True:
-	_,Image=camera.read()
-	cv2.imshow('Text detection',Image)
-	if cv2.waitKey(1)& 0xFF==ord('s'):
-		cv2.imwrite('test1.jpg',Image)
-		break
-camera.release()
-cv2.destroyAllWindows()
-with open("OCR1.py") as f:
-    exec(f.read())
-
+				file.write(result)
+				print(result)
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+speaker.Speak(result)
